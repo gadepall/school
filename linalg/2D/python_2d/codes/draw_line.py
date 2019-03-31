6be461b0723236bce6693b4f9c0f50033024cedd
gadepall <gadepall@gmail.com>
2019-03-31 20:58:49 +0530

    Add existing files

--- linalg/2D/python_2d/codes/draw_line.py
+++ linalg/2D/python_2d/codes/draw_line.py
@@ -1,59 +1,30 @@
-#Code by GVV Sharma
-#January 28, 2019
-#released under GNU GPL
+#Plotting AB
 import numpy as np
 import matplotlib.pyplot as plt
-from coeffs import *
 
 #if using termux
 import subprocess
 import shlex
 #end if
 
-#Generate line points
-def line_dir(m,c):
-  len =10
-  A = np.array([c,0]) 
-  x_AB = np.zeros((2,len))
-  lam_1 = np.linspace(-5,5,len)
-  for i in range(len):
-    temp1 = A + lam_1[i]*m
-    x_AB[:,i]= temp1.T
-  return x_AB
 
+A = np.array([-2,-2]) 
+B = np.array([1,3]) 
+len =10
 
-m1 = np.array([-1,1]) 
-m2 = np.array([1,3]) 
+x_AB = np.zeros((2,len))
+lam = np.linspace(0,1,len)
+for i in range(len):
+  temp1 = A + lam[i]*(B-A)
+  x_AB[:,i]= temp1.T
 
-c1 = 8
-c2 = 4
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.grid() # minor
 
-#Generating all lines
-line1 = line_dir(m1,c1)
-line2 = line_dir(m2,c2)
-
-#fig, ax = plt.subplots()
-#Plotting all lines
-plt.plot(line1[0,:],line1[1,:],label='$AB$')
-plt.plot(line2[0,:],line2[1,:],label='$BC$')
-
-#plt.plot(A[0], A[1], 'o')
-#plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
-#plt.plot(B[0], B[1], 'o')
-#plt.text(B[0] * (1 - 0.2), B[1] * (1) , 'B')
-#plt.plot(C[0], C[1], 'o')
-#plt.text(C[0] * (1 + 0.03), C[1] * (1 - 0.1) , 'C')
-
-plt.xlabel('$x$')
-plt.ylabel('$y$')
-plt.legend(loc='best')
-#Customize the major grid
-#ax.grid(which='major', linestyle='-', linewidth='0.5', color='red')
-# Customize the minor grid
-#plt.grid(which='minor', linestyle='-', linewidth='0.5', color='black')
-plt.grid(True, which='both')
-plt.minorticks_on
-#plt.grid(which='both') # minor
+plt.plot(A[0], A[1], 'o')
+plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A')
+plt.plot(B[0], B[1], 'o')
+plt.text(B[0] * (1 - 0.2), B[1] * (1) , 'B')
 
 #if using termux
 plt.savefig('../figs/draw_line.pdf')
@@ -61,10 +32,6 @@ plt.savefig('../figs/draw_line.eps')
 subprocess.run(shlex.split("termux-open ../figs/draw_line.pdf"))
 #else
 #plt.show()
-
-
-
-
 
 
 
