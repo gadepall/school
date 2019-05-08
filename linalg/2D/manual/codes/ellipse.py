ba3a94769872db4b5659cd09628b6f8a5620cd0c
gadepall <gadepall@gmail.com>
2019-05-08 13:35:18 +0530

    Add existing files

--- linalg/2D/manual/codes/ellipse.py
+++ linalg/2D/manual/codes/ellipse.py
@@ -11,7 +11,8 @@ import shlex
 #setting up plot
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
-len = 50000
+len = 100
+theta = np.linspace(0,2*np.pi,len)
 
 #Given ellipse parameters
 #Eqn : x.T@V@x + 2u.T@x+F=0
@@ -19,44 +20,37 @@ V = np.array(([5,-3],[-3,5]))
 u = np.array([11,-13])
 F = 29
 
-#Eqn in standard form : (x-c).T@V@(x-c)=K
-c = (-1 * LA.inv(V) @ u).reshape((2,1))
-K = c.T@V@c - F
-
-#From eqn (9.13), we have
-#lambda1*P1 = V*P1 , lambda2*P2 = V*P2
 eigval,eigvec = LA.eig(V)
-
+#
 D = np.diag(eigval)
 P = eigvec
 print("D=\n",D)
 print("P=\n",P)
-
-#Standard ellipse : y.T@D@y=1
-y1 = np.linspace(-1,1,len)
-y2 = np.sqrt(1.0 - D[0,0]*np.power(y1,2))/np.sqrt(D[1,1])
-y3 = -1*np.sqrt(1.0 - D[0,0]*np.power(y1,2))/np.sqrt(D[1,1])
-y = np.hstack((np.vstack((y1,y2)),np.vstack((y1,y3))))
+#Generating points on the circle C1
+y = np.zeros((2,len))
+y[0,:] = 1/eigval[0]*np.cos(theta)
+y[1,:] = 1/eigval[1]*np.sin(theta)
 
 #Plotting standard ellipse
-plt.plot(y[0,:],y[1,:],label='Std Ellipse')
+plt.plot(y[0,:],y[1,:],label='Ellipse at origin')
 
 #Affine Transformation
-#Equation : y = P.T@(x-c)/(K**0.5)
-x = (P @ (K**0.5 * y)) + c
-
+c = (-1 * LA.inv(V) @ u).reshape((2,1))
+print("c=\n",c)
+x = P@y + c
+#
 #Plotting required ellipse
-plt.plot(x[0,:],x[1,:],label='Ellipse E')
+plt.plot(x[0,:],x[1,:],label='Affine Ellipse')
 
 ax.plot()
 plt.xlabel('$x$');plt.ylabel('$y$')
 plt.legend(loc='best')
 plt.grid()
 
- #if using termux
+#if using termux
 plt.savefig('../figs/ellipse.pdf')
 plt.savefig('../figs/ellipse.eps')
 subprocess.run(shlex.split("termux-open ../figs/ellipse.pdf"))
- #else
+#else
 
 #plt.show()
