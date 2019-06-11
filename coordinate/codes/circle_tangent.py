6b33a79cba040c27ad895063a4eb25ae0395f014
gadepall <gadepall@gmail.com>
2019-06-11 17:22:10 +0530

    Add existing files

--- coordinate/codes/circle_tangent.py
+++
@@ -1,8 +0,0 @@
-#Importing numy, scipy, mpmath and pyplot
-import numpy as np
-
-theta = np.pi/3
-r = np.sqrt(2) + np.sqrt(np.sin(2*theta))
-print(r)
-print(oner)
-oner = np.sqrt(2) - np.sqrt(np.sin(2*theta))
---
+++ coordinate/twovar/codes/circle_tangent.py
@@ -0,0 +1,8 @@
+#Importing numy, scipy, mpmath and pyplot
+import numpy as np
+
+theta = np.pi/3
+r = np.sqrt(2) + np.sqrt(np.sin(2*theta))
+print(r)
+print(oner)
+oner = np.sqrt(2) - np.sqrt(np.sin(2*theta))
