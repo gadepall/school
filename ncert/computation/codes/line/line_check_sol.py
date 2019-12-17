b39d8ffcbc95c02b84bbc1bb904781e9a260bc49
gadepall <gadepall@gmail.com>
2019-12-17 18:59:13 +0530

    Add existing files

--- ncert/computation/codes/line/line_check_sol.py
+++ ncert/computation/codes/line/line_check_sol.py
@@ -15,9 +15,9 @@ import shlex
 
 
 #Lines
-n1 = np.array([1,3]) 
-n2 = np.array([2,-3]) 
-c =  np.array([6,12]) 
+n1 = np.array([1,2]) 
+n2 = np.array([2,4]) 
+c =  np.array([4,12]) 
 
 #Intercepts
 A1,B1 =  line_icepts(n1,c[0])
@@ -26,10 +26,12 @@ A2,B2 =  line_icepts(n2,c[1])
 
 #Matrix Ranks
 N=np.vstack((n1,n2))
-M =np.vstack((n1,n2, c))
+M =np.vstack((N.T, c)).T
+#M =np.vstack((n1,n2, c))
 rank_N = np.linalg.matrix_rank(N)
 rank_M = np.linalg.matrix_rank(M)
 m,n = np.shape(N)
+print(rank_M, rank_N, M)
 
 #Checking for solution
 if rank_N==rank_M:
@@ -42,9 +44,11 @@ else:
 
 ##Generating all lines
 k1 = 0
-k2 = 2
+k2 = 1
 
 x_A1B1 =  line_dir_pt(n1,A1,k1,k2)
+#k1 = -4
+#k2 = 4
 x_A2B2 =   line_dir_pt(n2,A2,k1,k2)
 #
 #Plotting all lines
@@ -58,9 +62,9 @@ plt.grid() # minor
 plt.axis('equal')
 
 #if using termux
-plt.savefig('./line/figs/line_check_sol2.pdf')
-plt.savefig('./line/figs/line_check_sol2.eps')
-subprocess.run(shlex.split("termux-open ./line/figs/line_check_sol2.pdf"))
+plt.savefig('./line/figs/line_check_sol.pdf')
+plt.savefig('./line/figs/line_check_sol.eps')
+subprocess.run(shlex.split("termux-open ./line/figs/line_check_sol.pdf"))
 #else
 #plt.show()
 
