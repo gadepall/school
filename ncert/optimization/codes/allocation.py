cb3d6e3744cb2f0f5863a1d7211ccf261c6c3a93
gadepall <gadepall@gmail.com>
2020-02-14 14:10:40 +0530

    Add existing files

--- ncert/optimization/codes/allocation.py
+++ ncert/optimization/codes/allocation.py
@@ -10,40 +10,24 @@ import shlex
 #end if
 
 #Line parameters
-A = np.array(( [3.0, 4.0], [1.0, 0.0 ], [0.0, 1.0 ])).T
-print(A)
-b = np.array([ 800.0, 0.0, 0.0 ]).reshape((3,-1))
+from cvxpy import *
+import numpy as np
 
-n =  np.array([1,1]).reshape(2,-1)
-#P = np.array([20,10]).reshape(2,-1)
-a=np.array([10500,9000]).reshape(2,-1)
+A = np.array(( [1, 1], [20, 10 ])).T
+b = np.array([ 50, 800 ]).reshape((2,-1))
+c = np.array([ 10500, 9000 ])
+
 x = Variable((2,1))
-#Cost function
-f =  a.T@x
+
+f = c @ x
 obj = Maximize(f)
-#Constraints
 
-constraints = [A.T@x <=b,n.T@x==10]
+constraints = [A.T @ x <= b]
 
-#solution
+
 Problem(obj, constraints).solve()
 
-print(f.value,x.value)
-
-
-#profit = p.LpProblem("profit_maximising_problem", p.LpMaximize)
+print("Number of hectares of land with X crop",x.value[0])
+print("Number of hectares of land with Y crop",x.value[1])
+print("Maximum profit =",f.value)
 #
-#x = p.LpVariable("x", lowBound=0)
-#y = p.LpVariable("y", lowBound=0)
-#
-#profit += 10500 * x + 9000 * y
-#profit += x + y == 50
-#profit += 20 * x + 10 * y <= 800
-#
-#print(profit)
-#status = profit.solve()
-#print(p.LpStatus[status])
-#print("Number of hectares in which x is grown:",p.value(x))
-#print("Number of hectares in which y is grown:",p.value(y))
-#print("Maximum Profit:",p.value(profit.objective))
-
