f27aafdeddf480916fb18b64ddb7d751600bb393
gadepall <gadepall@gmail.com>
2020-07-08 14:37:43 +0530

    Add existing files

--- programming/EE1083/jee/2016/octave_comp/codes/ee16b1004.m
+++ programming/EE1083/jee/2016/octave_comp/codes/ee16b1004.m
@@ -5,12 +5,13 @@ clc;
 x = 1e3;
 a=linspace(0,2,100);
 y=(1.+(a./(x))-(4./(x.^2))).^(2.*x);
-plot(a,y,'LineWidth',4);
+%plot(a,y,'LineWidth',4);
 z = e^3*ones(1,100);
-hold;
-plot(a,z,'ro','LineWidth',4);
-xlabel('a')
-ylabel('LHS and e^3')
-legend('LHS','e^3',"location","northeast")
-grid
-print -deps -color ee16b1004.eps 
+%hold;
+%plot(a,z,'ro','LineWidth',4);
+%xlabel('a')
+%ylabel('LHS and e^3')
+%legend('LHS','e^3',"location","northeast")
+%grid
+save ("-ascii", "test.dat", "a","y","z")
+%print -deps -color ee16b1004.eps 
