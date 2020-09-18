86793b3d3b494c8984365c2ac0a7f8e4b6b042f8
gadepall <gadepall@gmail.com>
2020-09-18 14:29:26 +0530

    Add existing files

---
+++ ncert/linalg/rncom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#cd /storage/emulated/0/tlc/school/loney/
+#tex2pdf.sh gvv_loney_coord
+
+cd /storage/emulated/0/tlc/school/ncert/geolin
+tex2pdf.sh gvv_ncert_geolin
+#rm -rf /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
+#svn co https://github.com/Adarsh1310/EE5609/trunk/Assignment_6 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
+
+cd /storage/emulated/0/tlc/school/ncert/linalg
