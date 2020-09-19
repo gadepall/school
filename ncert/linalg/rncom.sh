6530fdb8e02ac2dfdc63ed69c24e4477345e0be9
gadepall <gadepall@gmail.com>
2020-09-19 05:46:03 +0530

    Add existing files

--- ncert/linalg/rncom.sh
+++ ncert/linalg/rncom.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+
+#svn co https://github.com/shivangi-975/EE5609-Matrix_Theory/trunk/Assignment5 /storage/emulated/0/tlc/school/loney/solutions/13/11
 #cd /storage/emulated/0/tlc/school/loney/
 #tex2pdf.sh gvv_loney_coord
 
-cd /storage/emulated/0/tlc/school/ncert/geolin
-tex2pdf.sh gvv_ncert_geolin
-#rm -rf /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
-#svn co https://github.com/Adarsh1310/EE5609/trunk/Assignment_6 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
 
-cd /storage/emulated/0/tlc/school/ncert/linalg
+#svn co https://github.com/Adarsh1310/EE5609/trunk/Assignment_6 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
+#cd /storage/emulated/0/tlc/school/ncert/geolin
+#tex2pdf.sh gvv_ncert_geolin
+
+git add .
+git commit -m "Add existing files"
+git push origin
+
+
+#rm -rf /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
+
+
+#cd /storage/emulated/0/tlc/school/ncert/linalg
