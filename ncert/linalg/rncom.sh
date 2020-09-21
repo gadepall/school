88e0a10aa99f439e9214233fad6dad48f167150d
gadepall <gadepall@gmail.com>
2020-09-21 15:45:41 +0530

    Add existing files

--- ncert/linalg/rncom.sh
+++ ncert/linalg/rncom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#svn co https://github.com/yogi13995/yogesh_training/trunk/octave/MMSE/SNR_estimation  /storage/emulated/0/tlc/phylayer/codes/Octave/MMSE/21-09-20
+
 #git clone https://github.com/saurabh13002/EE5609 solutions/point_vector/18
 #svn co https://github.com/sahilsin/MatrixTheory/trunk/Assignment1 solutions/complex/11
 
@@ -18,7 +20,9 @@
 #tex2pdf.sh gvv_loney_coord
 
 
-#svn co https://github.com/shreeprasadbhat/matrix-theory/trunk/assignment4 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/14
+
+
+#svn co https://github.com/Hrithikraj2/MatrixTheory_EE5609/trunk/Assignment_3 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/29
 
 #cd /storage/emulated/0/tlc/school/ncert/geolin
 #tex2pdf.sh gvv_ncert_geolin
@@ -29,15 +33,18 @@
 
 #cd /storage/emulated/0/tlc/school/ncert/linman
 #tex2pdf.sh gvv_linalg_manual
-#python codes/pair/pair_test.py
+#python codes/line/skew_svd.py
 
 
-#svn co https://github.com/Adarsh1310/EE5609/trunk/Assignment_7 /storage/emulated/0/tlc/school/ncert/linexer/solutions/12
+
+#svn co https://github.com/harshachinta/EE5609-Matrix-Theory/trunk/Assignments/Assignment_7 /storage/emulated/0/tlc/school/ncert/linexer/solutions/7
+
+#svn co https://github.com/Adarsh1310/EE5609/trunk/Assignment_8 /storage/emulated/0/tlc/school/ncert/linexer/solutions/2/1
 
 #cd /storage/emulated/0/tlc/school/ncert/linexer
 #tex2pdf.sh gvv_linalg_exer
 
-
+#cd /storage/emulated/0/tlc/phylayer/
 cd /storage/emulated/0/tlc/school/
 git add .
 git commit -m "Add existing files"
@@ -47,7 +54,17 @@ git push origin
 #rm -rf /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/30
 
 
-#git clone https://github.com/Atul191/Assignment-2 /storage/emulated/0/tlc/school/ncert/linalg/solutions/matrix/7
 
+#svn co https://github.com/rubeenaafreen20/EE5600AI-ML/trunk/Assignment2 /storage/emulated/0/tlc/school/ncert/probability/solutions/1/1/4
+
+#cd /storage/emulated/0/tlc/school/ncert/probability/
+#tex2pdf.sh gvv_ncert_prob
+
+
+#git clone https://github.com/gadepall/aiml  /storage/emulated/0/tlc/aiml
+
+
+#svn co https://github.com/anshum0302/EE5609/trunk/assignment3 /storage/emulated/0/tlc/school/ncert/linalg/solutions/matrix/48
+#git clone https://github.com/SANDHYA-A/Assignment2 /storage/emulated/0/tlc/school/ncert/linalg/solutions/matrix/61
 #tex2pdf.sh gvv_ncert_linalg
 cd /storage/emulated/0/tlc/school/ncert/linalg
