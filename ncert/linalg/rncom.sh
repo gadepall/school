f95a1a2c2459639d395b09adcdd7a061312a4461
gadepall <gadepall@gmail.com>
2020-10-03 22:08:05 +0530

    Add existing files

--- ncert/linalg/rncom.sh
+++ ncert/linalg/rncom.sh
@@ -22,8 +22,9 @@
 #cd /storage/emulated/0/tlc/college/ugc/linalg/
 #tex2pdf.sh gvv_ugc_linalg
 
+#svn co https://github.com/pulkitsaxena92/EE20MTECH14016_MatrixEE5609/trunk/Assignment3 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/36
 
-#svn co https://github.com/sachinomdubey/Matrix-theory/trunk/Assignment5 /storage/emulated/0/tlc/school/ncert/geolin/solutions/1/9
+#svn co https://github.com/sachinomdubey/Matrix-theory/trunk/Assignment5 
 
 #cd /storage/emulated/0/tlc/school/ncert/geolin
 #tex2pdf.sh gvv_ncert_geolin
@@ -43,7 +44,9 @@
 
 #svn co https://github.com/utkarshsurwade/Matrix_Theory_EE5609/trunk/Assignment6 
 
-#svn co https://github.com/kranthiakssy/AI20RESCH14002_PhD_IITH/trunk/EE5609_Matrix_Theory/Assignment-8 /storage/emulated/0/tlc/school/ncert/linexer/solutions/2/32
+
+
+#svn co https://github.com/abhishekt711/EE5609/trunk/Assignment_4 
 
 
 #cd /storage/emulated/0/tlc/school/ncert/linexer
@@ -76,9 +79,9 @@ git push origin
 #git clone https://github.com/cs19resch11004/EE5600/  
 
 
+#svn co https://github.com/venkateshelangovan/IIT-Hyderabad-Assignments/trunk/Assignment12_Matrix_Theory /storage/emulated/0/tlc/college/hoffman/solutions/1/2/6
 
 
-#git clone https://github.com/satyam463/Assignment-7 /storage/emulated/0/tlc/college/hoffman/solutions/1/1/1
 
 #cd /storage/emulated/0/tlc/college/hoffman
 #tex2pdf.sh gvv_hoffman
@@ -97,8 +100,9 @@ git push origin
 #cd /storage/emulated/0/tlc/papers
 #git clone https://github.com/gadepall/papers
 
-#svn co https://github.com/EE20MTECH14019/EE5609/trunk/Assignment_6 /storage/emulated/0/tlc/school/dresden/solutions/4/45/1
+#svn co https://github.com/EE20MTECH14019/EE5609/trunk/Assignment_6 
 
+#git clone https://github.com/ankuraditya13/EE5609-Assignment8 /storage/emulated/0/tlc/school/dresden/solutions/4/45/2/1
 
 #cd /storage/emulated/0/tlc/school/dresden
 #tex2pdf.sh gvv_dresden
