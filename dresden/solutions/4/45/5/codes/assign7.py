846c9e92ac30e588998fd7c7cfb25dab2aac0d36
gadepall <gadepall@gmail.com>
2020-10-12 17:07:47 +0530

    Add existing files

---
+++ dresden/solutions/4/45/5/codes/assign7.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+from numpy.linalg import inv
+from numpy import linalg as LA
+
+M = np.array([[1,0],[0,1],[1,-1/2]])
+b = np.array([[-3],[1],[2]])
+
+#Singular Value Decomposition
+U, s, V=LA.svd(M)
+
+# Diagonalizing S
+S = np.zeros(M.shape)
+Sinv = S.T
+S[:2,:2] = np.diag(s)
+sinv = 1./s
+
+#Inverse transpose of S (Moore Penrose Pseudo Inverse)
+Sinv[:2,:2] = np.diag(sinv)
+
+#Solution
+x = V.T.dot(Sinv).dot(U.T).dot(b)
+print(x)
